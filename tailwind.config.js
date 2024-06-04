5528ea0680eb8877fd4d7fc35a0fe338bb6f1ee1
zidanabbas <zidan.abbas28@gmail.com>
2024-06-04 20:50:50 +0700

    add config style, padding container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      backgroundColor: {
+        Background: "#f2f7f5",
+        Paragraph: "#475d5b", //grey
+        Primary: "#00473e", //green
+        Secondary: "#ffa8ba", //pink
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
