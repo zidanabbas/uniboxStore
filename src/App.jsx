590800f2c52473d887be838a649508ce7f05aa5a
zidanabbas <zidan.abbas28@gmail.com>
2024-06-04 20:53:12 +0700

    feat : add page navigation routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
